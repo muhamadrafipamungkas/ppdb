1a8253a9deb5d845923856df46f81bdedf6a937e
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2023-05-28 06:13:55 +0700

    Add roles with default admin account

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('password'),
+            'roles' => 'admin',
+        ]);
     }
 }
