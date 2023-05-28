60da60d53e2472f8397f59457596ff7259007e21
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2023-05-28 13:51:15 +0700

    Create migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->unsignedInteger('nisn')->nullable();
             $table->enum('role', ['admin', 'user']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
