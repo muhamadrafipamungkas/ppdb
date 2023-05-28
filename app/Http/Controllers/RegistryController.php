cce9b98c11d4540e7f26445324b1ef483ae3f86f
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2023-05-28 13:57:28 +0700

    Add new controllers

---
+++ app/Http/Controllers/RegistryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Registry;
+use Illuminate\Http\Request;
+
+class RegistryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $registries = Registry::latest()->paginate(5);
+        return view('admin.registry.index',compact('registries'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.registry.create');
+    }
+}
