769c3e7ee23ef753fdfaebb89837ac17339c220b
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2023-05-28 08:06:32 +0700

    Create auto redirect router

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,13 +67,13 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Aplikasi Kotak Saran
+                    Suggestion Box's App
                 </div>
                 @if (Route::has('login'))
                 <div class="links">
-                    <a href="{{ route('login') }}">Masuk</a>
+                    <a href="{{ route('login') }}">Login</a>
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Daftar</a>
+                        <a href="{{ route('register') }}">Register</a>
                     @endif
                 </div>
                 @endif
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $user = \Illuminate\Support\Facades\Auth::user();
+    if ($user) {
+        if ($user->role == 'admin') {
+            return view('home');
+        } else if ($user->role == 'user') {
+            return view('home');
+        }
+    }
     return view('welcome');
 });
 
